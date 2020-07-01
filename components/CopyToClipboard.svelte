edd032b7fded10e4d075f1d4044541d2758089d0
SAMEER TARIQ <imsamtar@gmail.com>
2020-07-01 11:36:52 +0500

    Update: copytoclipboard

--- components/CopyToClipboard.svelte
+++ components/CopyToClipboard.svelte
@@ -1,11 +1,18 @@
 <script>
+  import { createEventDispatcher } from "svelte";
+
   export let element = undefined;
+
+  const dispatch = createEventDispatcher();
 
   function copy() {
     if (element) {
       element.select();
       element.setSelectionRange(0, 99999);
       document.execCommand("copy");
+      dispatch("copy", null);
+    } else {
+      dispatch("fial", null);
     }
   }
 </script>
